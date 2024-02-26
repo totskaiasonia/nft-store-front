aaa08c148edb0d2a420b2b3e74c9d23a623ba169
totskaiasonia <totskaiasonia@gmail.com>
2024-02-26 20:18:26 +0200

    Add year & price sliders in filters in store

--- src/_root/pages/Store.tsx
+++ src/_root/pages/Store.tsx
@@ -1,5 +1,7 @@
+
 import Category from '../../components/Category';
 import NftCardContainer from '../../components/NftCardContainer';
+import MySlider from '../../components/ui/MySlider';
 import styles from './Store.module.css';
 
 const Store = () => {
@@ -13,6 +15,34 @@ const Store = () => {
           <Category categoryName='Pixel'/>
           <Category categoryName='Art'/>
           <Category categoryName='Gaming'/>
+          <div className={styles.filters}>
+            <div style={{width: '200px'}}>
+              <p>Year</p>
+              <MySlider 
+                valueLabelDisplay="auto"
+                aria-label="year"
+                getAriaLabel={(index) =>
+                  index === 0 ? "Minimum year" : "Maximum year"
+                }
+                defaultValue={[2000, 2024]}
+                min={2000}
+                max={2024}
+              />
+            </div>
+            <div style={{width: '200px'}}>
+              <p>Price</p>
+              <MySlider 
+                valueLabelDisplay="auto"
+                aria-label="price"
+                getAriaLabel={(index) =>
+                  index === 0 ? "Minimum price" : "Maximum price"
+                }
+                defaultValue={[5, 4000]}
+                min={0.01}
+                max={10000}
+              />
+            </div>
+          </div>
         </div>
         <div className={styles.storeContent}>
           <h1>3D</h1>
