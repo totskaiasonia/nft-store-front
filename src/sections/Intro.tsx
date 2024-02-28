2df03b3d29d5e810276f347eed3eb2110df5a3ea
totskaiasonia <totskaiasonia@gmail.com>
2024-02-28 09:38:45 +0200

    Add serch input in intro section

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -1,20 +1,15 @@
-import AuthForm from '../components/AuthForm';
 import styles from './Intro.module.css';
 
 const Intro = () => {
   return (
     <>
         <div className={styles.introWrapper}>
-            <div className={styles.introForm}>
-              <AuthForm/>
-            </div>
-            <div className={styles.introTitleLayout}>
               <p className={styles.introTitle}>QUICK <span>NFT AUCTIONS</span> ON YOUR DEVICE</p>
+              <input className={styles.searchInput} placeholder='search'/>
               <button className={styles.introBtn}>learn more</button>
-            </div>
         </div>
     </>
   )
 }
 
-export default Intro
+export default Intro  
