12b48e831eaafb6c155db8dedffac5315f9d1a65
totskaiasonia <totskaiasonia@gmail.com>
2024-02-28 22:12:32 +0200

    changed search input styling in intro section

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -4,9 +4,12 @@ const Intro = () => {
   return (
     <>
         <div className={styles.introWrapper}>
-              <p className={styles.introTitle}>QUICK <span>NFT AUCTIONS</span> ON YOUR DEVICE</p>
-              <input className={styles.searchInput} placeholder='search'/>
-              <button className={styles.introBtn}>learn more</button>
+          <p className={styles.introTitle}>QUICK <span>NFT AUCTIONS</span> ON YOUR DEVICE</p>
+          <div className={styles.inputGroup}>
+            <label className={styles.inputGroup__label}>search</label>
+            <input className={styles.inputGroup__input} placeholder='Bored Ape'/>
+          </div>
+          <button className={styles.introBtn}>learn more</button>
         </div>
     </>
   )
