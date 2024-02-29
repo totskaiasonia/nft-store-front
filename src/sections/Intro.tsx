ed6406c9dfd97c889ba64f0fe5dcad17959862a3
totskaiasonia <totskaiasonia@gmail.com>
2024-02-29 21:27:24 +0200

    add categories in intro section

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -1,6 +1,20 @@
 import styles from './Intro.module.css';
+import Slider from "react-slick";
 
 const Intro = () => {
+
+  let settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 10,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 3000,
+    autoplaySpeed: 3000,
+    cssEase: "linear",
+    nextArrow: <></>,
+    prevArrow: <></>
+  };
   return (
     <>
         <div className={styles.introWrapper}>
@@ -9,6 +23,22 @@ const Intro = () => {
             <label className={styles.inputGroup__label}>search</label>
             <input className={styles.inputGroup__input} placeholder='Bored Ape'/>
           </div>
+          <Slider {...settings} className={styles.categoriesCorousel}>
+            <button className={styles.categoryBtn}><div className={styles.glows}></div>3D</button>
+            <button className={styles.categoryBtn}><div className={styles.glows}></div>Modern</button>
+            <button className={styles.categoryBtn}><div className={styles.glows}></div>West</button>
+            <button className={styles.categoryBtn}><div className={styles.glows}></div>Apes</button>
+            <button className={styles.categoryBtn}><div className={styles.glows}></div>Cars</button>
+            <button className={styles.categoryBtn}><div className={styles.glows}></div>Nature</button>
+            <button className={styles.categoryBtn}><div className={styles.glows}></div>True Pixel</button>
+            <button className={styles.categoryBtn}><div className={styles.glows}></div>Art</button>
+            <button className={styles.categoryBtn}><div className={styles.glows}></div>Disco</button>
+            <button className={styles.categoryBtn}><div className={styles.glows}></div>Glitch</button>
+            <button className={styles.categoryBtn}><div className={styles.glows}></div>City</button>
+            <button className={styles.categoryBtn}><div className={styles.glows}></div>Code</button>
+            <button className={styles.categoryBtn}><div className={styles.glows}></div>Game</button>
+            <button className={styles.categoryBtn}><div className={styles.glows}></div>Micro</button>
+          </Slider>
           <button className={styles.introBtn}>learn more</button>
         </div>
     </>
