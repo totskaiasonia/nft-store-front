97cc5c1a942ce663a443ea531f705926834f4fcd
totskaiasonia <totskaiasonia@gmail.com>
2024-03-07 17:07:09 +0200

    set up admin panel basic template

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import AdminJS from 'adminjs';
+import AdminJSExpress from '@adminjs/express';
+
 import express from "express";
 import mongoose from 'mongoose';
 
@@ -18,10 +21,14 @@ mongoose
 
 const app = express();
 
+const admin = new AdminJS({});
+const adminRouter = AdminJSExpress.buildRouter(admin);
+
 app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use(userRouter);
+app.use(admin.options.rootPath, adminRouter);
 
 app.get('/', (req, res) => {
     res.status(200).json({msg: "Root route"});
@@ -29,4 +36,5 @@ app.get('/', (req, res) => {
 
 app.listen(process.env.PORT | PORT, () => {
     console.log("Server listens on port", PORT);
+    console.log(`AdminJS started on http://localhost:${PORT}${admin.options.rootPath}`);
 });
